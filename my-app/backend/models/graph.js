aa6b84d4df90dda4b41e101fd8042bba9707c084
Saketh Renangi <rensaketh@gmail.com>
2021-11-29 22:49:19 -0500

    Finished basic graph functions, need to connect classes with database
    Finished basic graph functions, need to figure out how to connect the classes to the database to test. Also need to figure out how to solve running totals problem in main

--- my-app/backend/models/graph.js
+++ my-app/backend/models/graph.js
@@ -1,14 +1,23 @@
+const Movie = require('./movie');
 class graph {
     constructor() {
         this.adjList = new Map();
     }
 
     addVertex(newMovie) {
-        this.adjList.set(newMovie, []);
+        if(!this.adjList.has(newMovie)) {
+            this.adjList.set(newMovie, []);
+        }
     }
 
     addEdge(movieA, movieB) {
-        this.adjList.get(movieA).push(movieB);
-        this.adjList.get(movieB).push(movieA);
+        if(!this.adjList.get(movieA).includes(movieB)) {
+            this.adjList.get(movieA).push(movieB);
+        }
+        if(!this.adjList.get(movieB).includes(movieA)) {
+            this.adjList.get(movieB).push(movieA);
+        }
     }
+
+    
 }
--- my-app/backend/models/movie.js
+++ my-app/backend/models/movie.js
@@ -1,10 +1,9 @@
 class movie {
-    constructor(imdb_id, title, year, genres, duration, languages, directors, weighted_rating) {
+    constructor(imdb_id, title, year, genres, languages, directors, weighted_rating) {
         this.imdb_id = imdb_id;
         this.title = title;
         this.year = year;
         this.genres = genres;
-        this.duration = duration;
         this.languages = languages;
         this.directors = directors;
         this.weighted_rating = weighted_rating;
