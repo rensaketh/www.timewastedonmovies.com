0aa990e26e5c2bb0b40282fecd4d576adcede23b
Saketh Renangi <rensaketh@gmail.com>
2021-12-04 17:23:55 -0500

    Finished graph code, only issue is that it takes forever to run, need to come up with a solution (maybe hard limit of 5 edges)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,10 +9,11 @@ const Movie = require('./components/data structures/movie');
 
 class graph {
     constructor() {
-        this.adjList = new Map();
+        this.adjList = new Map([[String.prototype, [String.prototype]]]);
     }
 
-    static addVertex(newMovie) {
+    addVertex(newMovie) {
+        //console.log(newMovie.type);
         if (!this.adjList.has(newMovie)) {
             this.adjList.set(newMovie, []);
         }
@@ -30,31 +31,35 @@ class graph {
 }
 
 function App() {
-    console.log("Hi");
+    //console.log("Hi");
     axios.get('/movies')
         .then((response) => {
-            console.log("hi");
+            //console.log("hi");
             //console.log(response.data);
             //console.log(response.status);
             //console.log(response.statusText);
             //console.log(response.headers);
             //console.log(response.config);
-            let database = response.data
+            let database = response.data;
             function intersection(firstItem, secondItem) {
-                let compareSet = new Set(secondItem);
-                return firstItem.filter(item => compareSet.has(item));
+                let setA = new Set(firstItem.split(', '));
+                let setB = new Set(secondItem.split(', '));
+                let intersection = new Set([...setA].filter(x => setB.has(x)));
+                return Array.from(intersection);
             }
 
             function calculateWeight(movieA, movieB) {
-                let common = intersection(movieA.genres, movieB.genres);
-                let weight = 100 - 10 * (movieA.genres.size - common.size) - 0.5 * (Math.abs(movieA.weighted_rating - movieB.weighted_rating)) - 8;
-                if (movieA.director == movieB.director) {
+                let common = intersection(movieA.genre, movieB.genre);
+                let diffGenre = movieA.genre.split(', ').length - common.length;
+                let diffRating = Math.abs(parseFloat(movieA.weighted_average_vote) - parseFloat(movieB.weighted_average_vote));
+                let weight = 100 - (10 * diffGenre) - (0.5 * diffRating) - 8;
+                if (intersection(movieA.director,movieB.director).length > 0) {
                     weight += 3;
                 }
-                if (movieA.language == movieB.language) {
+                if (movieA.language[0] == movieB.language[0]) {
                     weight += 5;
                 }
-                if (Math.abs(movieA.year - movieB.year) > 10) {
+                if (Math.abs(parseInt(movieA.year) - parseInt(movieB.year)) > 10) {
                     weight -= 8;
                 }
                 return weight;
@@ -64,35 +69,39 @@ function App() {
 
             for (let i = 0; i < database.length; i++) {
                 let movieA = database[i];
-                console.log(movieA);
+                //console.log(movieA);
                 let lowestWeight = 150;
-                let lowestMovie;
                 let highestWeightedMovies = [];
-                console.log(movieA.imdb_title_id);
-                graph.addVertex(movieA.imdb_title_id);
+                let movieIDA = movieA.imdb_title_id;
+                //console.log(typeof movieIDA);
+                movieGraph.addVertex(movieIDA);
                 for (let j = i + 1; j < database.length; j++) {
                     let movieB = database[j];
                     let weight = calculateWeight(movieA, movieB);
 
                     movieGraph.addVertex(movieB.imdb_title_id);
 
+                    let adjacentMovies = {
+                        adjacentID: movieB.imdb_title_id,
+                        adjacentWeight: weight
+                    };
                     //need to figure out how to keep running total of 5 maximum weights
-                    if (highestWeightedMovies.size < 5) {
-                        let adjacentMovies = {
-                            adjacentWeight: weight,
-                            adjacentID: movieB.ID
-                        };
-                        highestWeightedMovies.push(movieB);
-                        if (lowestWeight > weight) {
+                    if (highestWeightedMovies.length < 5) {
+                        if(lowestWeight > weight) {
                             lowestWeight = weight;
-                            lowestMovie = movieB;
                         }
-                    } else {
+                        highestWeightedMovies.push(adjacentMovies);
+                        highestWeightedMovies.sort((a, b) => (a.adjacentWeight > b.adjacentWeight) ? 1 : -1);
+                    } else if(weight > lowestWeight) {
                         //if(weight > lowestWeight) need to replace a movie in the list
+                        highestWeightedMovies.shift();
+                        highestWeightedMovies.push(adjacentMovies);
+                        highestWeightedMovies.sort((a, b) => (a.adjacentWeight > b.adjacentWeight) ? 1 : -1);
+                        lowestWeight = highestWeightedMovies.at(-1).adjacentWeight;
                     }
                 }
                 for (const element of highestWeightedMovies) {
-                    movieGraph.addEdge(movieA, element);
+                    movieGraph.addEdge(movieIDA, element.adjacentID);
                 }
             }
         });
